4461d75685f9e2405237837bfd998a306c337211
Tom <emailfortomg@gmail.com>
2020-03-23 19:15:54 +0000

    Made Package Version Checking Occur Between Updating Packages

--- src/superUpdater.js
+++ src/superUpdater.js
@@ -22,51 +22,46 @@ const getPackageVersion = async function (name) {
     parse: "json",
   });
 
+  if (data.body.error === "Not found") {
+    throw new Error("PACKAGE-404")
+  }
+
   return data.body["dist-tags"].latest;
 };
 
 const updatePackages = async function (packages) {
   // get all package versions
-  let newPackages = [];
-  packages.forEach((package) => {
-    newPackages.push(
-      new Promise(function (resolve, reject) {
-        getPackageVersion(package.name)
-          .then((version) => {
-            package.version = version;
-            resolve(package);
-          })
-          .catch((error) => {
-            console.log(chalk.red(`Skipping ${package.name} - couldn't find it in the NPM registry.`));
-            resolve(error);
-          });
-      })
-    );
-  });
-
-  newPackages = await Promise.all(newPackages);
-  newPackages = newPackages.filter((promise) => !(promise instanceof Error));
-
-  newPackages.forEach((newPackage) => {
-    queue.add(
-      () =>
-        new Promise(function (resolve, reject) {
-          if (newPackage.current === newPackage.version) {
-            console.log(`"${newPackage.name}" is up to date with version ${newPackage.current}`);
+  const updatePackage = function(package) {
+    new Promise(function (resolve, reject) {
+      getPackageVersion(package.name)
+        .then((version) => {
+          if (package.current === version) {
+            console.log(`"${package.name}" is up to date with version ${package.current}`);
             resolve();
           } else {
-            console.log(chalk.blue(`Updating "${newPackage.name}" version ${newPackage.current} to version ${newPackage.version}`));
-            runCommand(`npm install ${newPackage.name}@${newPackage.version}`)
+            console.log(chalk.blue(`Updating "${package.name}" version ${package.current} to version ${version}`));
+            runCommand(`npm install ${package.name}@${version}`)
               .then(() => {
-                console.log(chalk.green(`Updated "${newPackage.name}" to version ${newPackage.version}`));
+                console.log(chalk.green(`Updated "${package.name}" to version ${version}`));
                 resolve();
               })
               .catch((error) => {
-                console.log(`An error occurred while attempting to update ${newPackage.name}`);
+                console.log(`An error occurred while attempting to update ${package.name}`);
+                reject(error);
               });
           }
         })
-    );
+        .catch((error) => {
+          if (error.message === "PACKAGE-404" || error.message === "Cannot read property 'latest' of undefined") {
+            console.log(chalk.red(`Skipping ${package.name} - couldn't find it in the NPM registry.`));
+          }
+          else reject(error);
+        });
+    });
+  };
+
+  packages.forEach((package) => {
+    queue.add(() => updatePackage(package));
   });
 };
 
